4ffc70570fb62ff94d208f20d91d9a67bbdb306d
rod <rodrip0129@gmail.com>
2023-10-25 10:57:43 -0300

    add categories controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, controllers: { registrations: 'registrations' }
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
+  resources :categories
 end
