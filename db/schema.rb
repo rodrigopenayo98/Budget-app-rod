bea50fd551862576f0bf56cb710a177049b9c713
rod <rodrip0129@gmail.com>
2023-10-24 23:01:17 -0300

    fix linters

--- db/schema.rb
+++ db/schema.rb
@@ -10,47 +10,47 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_10_25_012443) do
+ActiveRecord::Schema[7.0].define(version: 20_231_025_012_443) do
   # These are extensions that must be enabled in order to support this database
-  enable_extension "plpgsql"
+  enable_extension 'plpgsql'
 
-  create_table "categories", force: :cascade do |t|
-    t.string "icon"
-    t.string "name"
-    t.integer "author_id"
-    t.bigint "user_id", null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["user_id"], name: "index_categories_on_user_id"
+  create_table 'categories', force: :cascade do |t|
+    t.string 'icon'
+    t.string 'name'
+    t.integer 'author_id'
+    t.bigint 'user_id', null: false
+    t.datetime 'created_at', null: false
+    t.datetime 'updated_at', null: false
+    t.index ['user_id'], name: 'index_categories_on_user_id'
   end
 
-  create_table "transaction_categories", force: :cascade do |t|
-    t.bigint "transaction_id", null: false
-    t.bigint "category_id", null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["category_id"], name: "index_transaction_categories_on_category_id"
-    t.index ["transaction_id"], name: "index_transaction_categories_on_transaction_id"
+  create_table 'transaction_categories', force: :cascade do |t|
+    t.bigint 'transaction_id', null: false
+    t.bigint 'category_id', null: false
+    t.datetime 'created_at', null: false
+    t.datetime 'updated_at', null: false
+    t.index ['category_id'], name: 'index_transaction_categories_on_category_id'
+    t.index ['transaction_id'], name: 'index_transaction_categories_on_transaction_id'
   end
 
-  create_table "transactions", force: :cascade do |t|
-    t.string "name"
-    t.decimal "amount"
-    t.integer "author_id"
-    t.bigint "user_id", null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["user_id"], name: "index_transactions_on_user_id"
+  create_table 'transactions', force: :cascade do |t|
+    t.string 'name'
+    t.decimal 'amount'
+    t.integer 'author_id'
+    t.bigint 'user_id', null: false
+    t.datetime 'created_at', null: false
+    t.datetime 'updated_at', null: false
+    t.index ['user_id'], name: 'index_transactions_on_user_id'
   end
 
-  create_table "users", force: :cascade do |t|
-    t.string "name"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+  create_table 'users', force: :cascade do |t|
+    t.string 'name'
+    t.datetime 'created_at', null: false
+    t.datetime 'updated_at', null: false
   end
 
-  add_foreign_key "categories", "users"
-  add_foreign_key "transaction_categories", "categories"
-  add_foreign_key "transaction_categories", "transactions"
-  add_foreign_key "transactions", "users"
+  add_foreign_key 'categories', 'users'
+  add_foreign_key 'transaction_categories', 'categories'
+  add_foreign_key 'transaction_categories', 'transactions'
+  add_foreign_key 'transactions', 'users'
 end
