d5d1c5b36460bee65061202899341dc7eb5afb80
rod <rodrip0129@gmail.com>
2023-10-25 12:04:21 -0300

    update models

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,9 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
+  root to: 'users#index'
   # root "articles#index"
-  resources :categories
+  resources :categories do
+    resources :transactions
+  end
 end
