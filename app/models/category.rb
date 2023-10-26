e84b88dc76b2f60d31c73f7a908091ff31093224
rod <rodrip0129@gmail.com>
2023-10-25 22:28:58 -0300

    add views for transactions

--- app/models/category.rb
+++ app/models/category.rb
@@ -2,11 +2,16 @@ class Category < ApplicationRecord
   belongs_to :user, class_name: 'User', foreign_key: 'author_id'
   has_many :related_transactions, through: :transaction_categories, source: :related_transaction
   has_many :transaction_categories
+  has_many :transactions
 
   validates :icon, presence: true
   validates :name, presence: true
 
   def total_amount
-    related_transactions.sum(:amount)
+    total = 0
+    transactions.each do |transaction|
+      total += transaction.amount.to_i
+    end
+    total
   end
 end
