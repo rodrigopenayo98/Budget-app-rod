4b3f070889c60484bb564f916b10e76ffdd1920e
rod <rodrip0129@gmail.com>
2023-10-27 12:14:25 -0300

    add test to splash

---
+++ spec/splash_view_spec.rb
@@ -0,0 +1,14 @@
+require 'rails_helper'
+
+RSpec.describe 'splash_screen/index.html.erb', type: :view do
+  it 'checks the structure of the splash view' do
+    render
+
+    expect(rendered).to have_selector('.splash-container')
+    expect(rendered).to have_selector('.header-splash')
+    expect(rendered).to have_selector('.title-splash', text: 'Welcome to Budget App Rod!')
+    expect(rendered).to have_selector('.links-container-splash')
+    expect(rendered).to have_link('Sign Up', href: new_user_registration_path, class: 'sign-btn')
+    expect(rendered).to have_link('Log In', href: new_user_session_path, class: 'log-btn')
+  end
+end
