f353d7233425810e172b995163df1c977c4896d7
rod <rodrip0129@gmail.com>
2023-10-28 00:21:17 -0300

    update tests for new code

--- spec/splash_view_spec.rb
+++ spec/splash_view_spec.rb
@@ -5,10 +5,10 @@ RSpec.describe 'splash_screen/index.html.erb', type: :view do
     render
 
     expect(rendered).to have_selector('.splash-container')
-    expect(rendered).to have_selector('.header-splash')
-    expect(rendered).to have_selector('.h1-splash', text: 'Welcome to Budget App Rod!')
+    expect(rendered).to have_selector('.content-splash')
+    expect(rendered).to have_selector('.h2-splash', text: 'Welcome to Budget App Rod!')
     expect(rendered).to have_selector('.links-container-splash')
-    expect(rendered).to have_link('Sign Up', href: new_user_registration_path, class: 'sign-btn')
-    expect(rendered).to have_link('Log In', href: new_user_session_path, class: 'log-btn')
+    expect(rendered).to have_link('LOG IN', href: new_user_session_path, class: 'log-btn')
+    expect(rendered).to have_link('SIGN UP', href: new_user_registration_path, class: 'sign-btn')
   end
 end
